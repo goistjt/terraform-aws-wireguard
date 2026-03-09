a36276274dddb81d57ce280aa33cf306c97c7693
Jeremiah Goist <jgoist@edgesource.com>
2026-03-09 13:09:24 -0400

    Add asg group name to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,7 @@ output "vpn_asg_name" {
   value       = aws_autoscaling_group.wireguard_asg.name
   description = "ID of the internal Security Group to associate with other resources needing to be accessed on VPN."
 }
+
+output "asg_group_name" {
+  value = aws_autoscaling_group.wireguard_asg.name
+}
